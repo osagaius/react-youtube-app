60f7d51eb1d01b8e32c46bce936fdda89fb144b5
Gaius-Obaseki, Osayame <oobaseki@cardlytics.com>
2016-04-23 13:40:38 -0400

    Added empy components. Completed basic search bar skeleton

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Component } from 'react';
+import SearchBar from './search_bar';
+
+const API_KEY = 'AIzaSyCvppKt9YXvdEjjVzWFMdcI05cuAHuKQ7A';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar/>
+      </div>
     );
   }
 }
